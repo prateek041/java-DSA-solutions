cad4a21242ee6a6f31eaa71d633d89aac42311ee
prateek041 <prateeksingh9741@gmail.com>
2022-04-24 12:15:11 +0530

    setZeroes : successully positioned the zeros

--- arrays/src/com/company/setMatrixZero.java
+++ arrays/src/com/company/setMatrixZero.java
@@ -2,8 +2,21 @@ package com.company;
 
 public class setMatrixZero {
     public static void main(String[] args) {
-
+        int[][] matrix = {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        setZeroes(matrix);
     }
 
-    
+    public static void setZeroes(int[][] matrix) {
+        int elementCount = (matrix.length) * (matrix[0].length);
+        while (elementCount != 0) {
+            for (int row = 0; row < matrix.length; row++) {
+                for (int column = 0; column < matrix[0].length; column++) {
+                    if (matrix[row][column] == 0) {
+                        System.out.printf("zero found at %d row and %d column \n", row, column);
+                    }
+                    elementCount--;
+                }
+            }
+        }
+    }
 }
