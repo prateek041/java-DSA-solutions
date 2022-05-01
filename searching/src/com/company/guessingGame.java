411a9e682e0143df61abda599cd61036fd49a02f
prateek041 <prateeksingh9741@gmail.com>
2022-05-01 11:08:44 +0530

    added guessingGame

---
+++ searching/src/com/company/guessingGame.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class guessingGame {
+    public static void main(String[] args) {
+
+    }
+
+    public class Solution extends GuessGame {
+        public int guessNumber(int n) {
+            int ans = 0;
+            int start = 1;
+            int end = n;
+            while (start <= end){
+                int mid = start+(end-start)/2;
+                int result = guess(mid);
+                if (result == 0){
+                    ans = mid;
+                    break;
+                }
+                else if (result == -1){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            return ans;
+        }
+    }
+}
