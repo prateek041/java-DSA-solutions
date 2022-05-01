04d748bf054c3620b26450d4ae739ecbc125194b
prateek041 <prateeksingh9741@gmail.com>
2022-05-01 12:15:42 +0530

    added searchRange

---
+++ searching/src/com/company/SearchRange.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class SearchRange {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int [] nums, int target){
+        int firstAppearance = -1;
+        int lastAppearance = -1;
+        int[] position = new int[2];
+        int start = 0;
+        int end = nums.length-1;
+        while (start <= end){
+            int mid = start+(end-start)/2;
+            if (nums[start] == target){
+                firstAppearance = start;
+            }
+            if (nums[end] == target){
+                lastAppearance = end;
+            }
+            if(nums[mid] > target){
+                end = mid-1;
+            }
+            else if(nums[mid] < target){
+                start = mid+1;
+            }
+            else{
+                if(firstAppearance == -1){
+                    start++;
+                }
+                else if(lastAppearance == -1){
+                    end--;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        position[0] = firstAppearance;
+        position[1] = lastAppearance;
+        return position;
+    }
+}
--- searching/src/com/company/guessingGame.java
+++ searching/src/com/company/guessingGame.java
@@ -5,26 +5,26 @@ public class guessingGame {
 
     }
 
-    public class Solution extends GuessGame {
-        public int guessNumber(int n) {
-            int ans = 0;
-            int start = 1;
-            int end = n;
-            while (start <= end){
-                int mid = start+(end-start)/2;
-                int result = guess(mid);
-                if (result == 0){
-                    ans = mid;
-                    break;
-                }
-                else if (result == -1){
-                    end = mid-1;
-                }
-                else{
-                    start = mid+1;
-                }
-            }
-            return ans;
-        }
-    }
+//    public class Solution extends GuessGame {
+//        public int guessNumber(int n) {
+//            int ans = 0;
+//            int start = 1;
+//            int end = n;
+//            while (start <= end){
+//                int mid = start+(end-start)/2;
+//                int result = guess(mid);
+//                if (result == 0){
+//                    ans = mid;
+//                    break;
+//                }
+//                else if (result == -1){
+//                    end = mid-1;
+//                }
+//                else{
+//                    start = mid+1;
+//                }
+//            }
+//            return ans;
+//        }
+//    }
 }
